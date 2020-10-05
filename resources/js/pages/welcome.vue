65f2592e6485b2d4fd2457aeeb19daaea33a6666
unknown <doodegre@gmail.com>
2020-10-05 18:44:38 +0800

    add tailwind

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -17,13 +17,11 @@
     </div>
 
     <div class="text-center">
-      <div class="title mb-4">
-        {{ title }}
-      </div>
-
-      <div class="links">
-        <a href="https://github.com/cretueusebiu/laravel-vue-spa">github.com/cretueusebiu/laravel-vue-spa</a>
-      </div>
+        <div class="flex justify-start bg-gray-200">
+          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+          <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+        </div>
     </div>
   </div>
 </template>
